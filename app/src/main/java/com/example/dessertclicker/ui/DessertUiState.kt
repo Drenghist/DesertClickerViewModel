15e8315baa71bac2c38429e8e8eed147525632b0
Alex <vrodal@gmail.com>
2024-11-21 18:11:55 +0100

    1 -- Primera versión 2 -- Creo las dos primeras clases de ViewModel 3 y 4 -- Inicializo las instancias en la mainclass 5 6 7 8 -- Modificado 9 -- La barra Corregido problema de iniciación en DessertUiState.kt

--- app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
+++ app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
@@ -4,11 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import com.example.dessertclicker.data.Datasource
 
 data class DessertUiState(
     var revenue :Int = 0,
     var dessertsSold : Int = 0,
     val currentDessertIndex : Int = 0,
-    var currentDessertPrice : Int = 0,
-    var currentDessertImageId : Int = 0,
+    var currentDessertPrice : Int = Datasource.dessertList[currentDessertIndex].price,
+    var currentDessertImageId : Int = Datasource.dessertList[currentDessertIndex].imageId,
 )
